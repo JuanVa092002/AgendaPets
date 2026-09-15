6e88743f16490088e7d0cf7c4e1fba38f2b481d1
JuanVa092002 <juan.pastasvalencia@hotmail.com>
2026-09-14 23:16:23 -0500

    Quita el usuario in-memory de Spring en el perfil de producción.

--- apps/server/src/main/java/com/agendapets/agendapets/AgendapetsApplication.java
+++ apps/server/src/main/java/com/agendapets/agendapets/AgendapetsApplication.java
@@ -2,8 +2,9 @@ package com.agendapets.agendapets;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
 public class AgendapetsApplication {
 
 	public static void main(String[] args) {
